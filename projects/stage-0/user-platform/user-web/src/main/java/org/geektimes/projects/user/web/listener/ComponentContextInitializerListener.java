dd204f165f47f84af9035afb55d6f0f3a5a9f7df
solitary <1026197530@qq.com>
2021-03-17 20:07:38 +0800

    增加jolokia整合实现

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -1,10 +1,15 @@
 package org.geektimes.projects.user.web.listener;
 
 import org.geektimes.context.ComponentContext;
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.management.UserManager;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
 
 /**
  * {@link ComponentContext} 初始化器
@@ -19,12 +24,26 @@ public class ComponentContextInitializerListener implements ServletContextListen
         this.servletContext = sce.getServletContext();
         ComponentContext context = new ComponentContext();
         context.init(servletContext);
+//    }
+        testJMX();
     }
 
+    private void testJMX() {
+        try {
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            ObjectName objectName = new ObjectName("org.geektimes.projects.user.management.UserManger:type=UserManager");
+            User user = new User();
+            UserManager userManager = new UserManager(user);
+            server.registerMBean(userManager, objectName);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
 //        ComponentContext context = ComponentContext.getInstance();
 //        context.destroy();
     }
+
 
 }
